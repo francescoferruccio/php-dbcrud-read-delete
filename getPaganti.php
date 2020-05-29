b644b6beb08a75f890b7b91e1178cf966e561df2
francesco <francesco.ferruccio88@gmail.com>
2020-05-29 15:20:35 +0200

    stampa lista paganti

--- getPaganti.php
+++ getPaganti.php
@@ -1,1 +1,36 @@
 <?php
+
+  header('Content-Type: application/json');
+
+  $server = 'localhost';
+  $username = 'root';
+  $password = 'root';
+  $dbName = 'hoteldb';
+
+  $conn = new mysqli($server, $username, $password, $dbName);
+
+  if ($conn -> connect_errno) {
+    echo $conn -> connect_errno;
+    return;
+  }
+
+  $sql = '
+    SELECT id, name, lastname, address
+    FROM paganti
+  ';
+
+  $results = $conn -> query($sql);
+
+  if ($results -> num_rows < 1) {
+    echo 'no results';
+    return;
+  }
+
+  $res = [];
+
+  while ($row = $results -> fetch_assoc()) {
+    $res[] = $row;
+  }
+  $conn -> close();
+
+  echo json_encode($res);
--- main.js
+++ main.js
@@ -2,9 +2,31 @@
 // Mostrare tutte le informazioni interessanti dell'entita' e dare la
 // possibilita' all'utente di elminare un pagante dalla tabella e quindi
 // dal front-end
+function printPaganti(paganti) {
+  var source = $("#pagante-template").html();
+  var template = Handlebars.compile(source);
+
+  for (var pagante of paganti) {
+    var html = template(pagante);
+    $('#container').append(html);
+  }
+}
+
+function getPaganti() {
+  $.ajax({
+    url: 'getPaganti.php',
+    method: 'GET',
+    success: function(data) {
+      printPaganti(data);
+    },
+    error: function(err) {
+      console.error(err);
+    }
+  });
+}
 
 function init() {
-  console.log('hello world');
+  getPaganti();
 }
 
 $(document).ready(init);
