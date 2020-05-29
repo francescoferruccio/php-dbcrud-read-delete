abdba9deca8cc331c9d9f1df41058c628a99ef29
francesco <francesco.ferruccio88@gmail.com>
2020-05-29 14:49:20 +0200

    test commit da linux

---
+++ main.js
@@ -0,0 +1,10 @@
+// Creare una index + delete sulla tabella paganti.
+// Mostrare tutte le informazioni interessanti dell'entita' e dare la
+// possibilita' all'utente di elminare un pagante dalla tabella e quindi
+// dal front-end
+
+function init() {
+  console.log('hello world');
+}
+
+$(document).ready(init);
